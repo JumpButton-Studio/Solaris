8bb7299700a054d6e7d54bb735802c372db9e6d8
Matthew Auld <matthew@jumpbuttonstudio.com>
2016-06-03 19:51:19 -0400

    Graphics Engine Running
    Got the basic graphics processing done with now FPS limiter in place
    yet.

--- Solaris Client/src/net/matthewauld/solaris/client/Client.java
+++ Solaris Client/src/net/matthewauld/solaris/client/Client.java
@@ -8,9 +8,52 @@
  */
 package net.matthewauld.solaris.client;
 
-/**
- *
- */
-public class Client {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 
+import javax.swing.JFrame;
+
+public class Client extends Canvas implements Runnable {
+	public static final int		CLIENT_HEIGHT		= 720;
+	public static final int		CLIENT_WIDTH		= 1280;
+	public static final String	GAME_NAME			= "Nogard";
+	private static boolean		GAME_RUNNING		= true;
+	private static final long	serialVersionUID	= 1965121834424532560L;
+
+	public static void main(String[] args) {
+		new Client();
+	}
+
+	private JFrame window;
+
+	public Client() {
+		// Start Game Window
+		window = new JFrame(GAME_NAME + " ~ Solaris Engine");
+		window.setSize(CLIENT_WIDTH, CLIENT_HEIGHT);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(this);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		createBufferStrategy(2);
+		// Start Logic & Graphic Loop
+		new Thread(this).start();
+	}
+
+	public void render(Graphics2D g) {
+		g.setColor(Color.CYAN);
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	@Override
+	public void run() {
+
+		BufferStrategy bs = getBufferStrategy();
+		while (GAME_RUNNING) {
+			render((Graphics2D) bs.getDrawGraphics());
+			bs.show();
+
+		}
+	}
 }
