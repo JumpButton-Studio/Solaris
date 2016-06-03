375f0cc1e1479fef3827bba8f4a3537ba895aae8
Matthew Auld <matthew@jumpbuttonstudio.com>
2016-06-03 18:51:38 -0400

    Initial Setup
    Setup the development environment and getting ready to start Project
    Solaris.

---
+++ Solaris Client/src/net/matthewauld/solaris/client/Client.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Lycrios
+ * Package: net.matthewauld.solaris.client
+ * File: Client.java
+ * Created: Jun 3, 2016
+ * Copyrighted 2016 (c)
+ * ALL RIGHTS RESERVED
+ */
+package net.matthewauld.solaris.client;
+
+/**
+ *
+ */
+public class Client {
+
+}
