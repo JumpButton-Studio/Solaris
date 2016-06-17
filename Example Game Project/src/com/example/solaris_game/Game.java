78740983e8c73f6dea6dd0dca01ba636baa3245a
Matthew Auld <matthew@1workbook.com>
2016-06-17 01:24:36 -0400

    Implemented Logging System

--- Example Game Project/src/com/example/solaris_game/Game.java
+++ Example Game Project/src/com/example/solaris_game/Game.java
@@ -8,10 +8,15 @@
  */
 package com.example.solaris_game;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import net.matthewauld.solaris.client.Client;
+import net.matthewauld.solaris.common.Log;
 
 public class Game extends Client {
-	private static final long serialVersionUID = -9142163124484779291L;
+	private static final long	serialVersionUID	= -9142163124484779291L;
 
 	public static void main(String[] args) {
 		setGameName("Nogard Online");
@@ -20,6 +25,12 @@ public class Game extends Client {
 	}
 
 	public Game() {
+		Log.TRACE();
+		Log.trace("Trace Label");
+		Log.debug("Debug Label");
+		Log.info("Info Label");
+		Log.warn("Warning Label");
+		Log.error("Error Label");
 		try {
 			this.loadingScreen.start();
 		} catch (Exception e) {
@@ -28,4 +39,18 @@ public class Game extends Client {
 		}
 	}
 
+	@Override
+	public void render(Graphics2D g) {
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		int gg = (int) (Math.random() * 255);
+		int rr = (int) (Math.random() * 255);
+		int bb = (int) (Math.random() * 255);
+		g.setColor(new Color(rr, gg, bb));
+		g.fillArc(0, 0, 100, 100, 0, 360);
+	}
+
+	@Override
+	public void updateGame() {
+		Log.trace("tick");
+	}
 }
--- Solaris Client/src/net/matthewauld/solaris/client/Client.java
+++ Solaris Client/src/net/matthewauld/solaris/client/Client.java
@@ -11,7 +11,12 @@ package net.matthewauld.solaris.client;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferStrategy;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -20,18 +25,15 @@ import net.matthewauld.solaris.client.assets.Asset;
 import net.matthewauld.solaris.client.assets.AssetType;
 import net.matthewauld.solaris.client.screen.LoadingScreen;
 import net.matthewauld.solaris.client.screen.Screen;
+import net.matthewauld.solaris.common.Log;
 
-public class Client extends Canvas implements Runnable {
+public abstract class Client extends Canvas implements Runnable {
 	private static Color		BASE_COLOR			= Color.CYAN;
 	public static int			CLIENT_HEIGHT		= 720;
 	public static int			CLIENT_WIDTH		= 1280;
 	public static String		GAME_NAME			= "EXAMPLE GAME";
 	private static boolean		GAME_RUNNING		= true;
 	private static final long	serialVersionUID	= 1965121834424532560L;
-
-	public static void main(String[] args) {
-		new Client();
-	}
 
 	public static void setBaseColor(Color base) {
 		BASE_COLOR = base;
@@ -47,8 +49,10 @@ public class Client extends Canvas implements Runnable {
 	}
 
 	private ArrayList<Asset>	assets	= new ArrayList<Asset>();
+
 	protected LoadingScreen		loadingScreen;
 	private ArrayList<Screen>	screens	= new ArrayList<Screen>();
+	private BufferStrategy		strategy;
 	private JFrame				window;
 
 	public Client() {
@@ -67,11 +71,13 @@ public class Client extends Canvas implements Runnable {
 	}
 
 	/**
-	 * All graphics will be process through this function.
+	 * Do all graphic rendering here.
 	 *
 	 * @param g
 	 */
-	public void render(Graphics2D g) {
+	public abstract void render(Graphics2D g);
+
+	private void renderSystem(Graphics2D g) {
 		g.setColor(BASE_COLOR);
 		g.fillRect(0, 0, getWidth(), getHeight());
 
@@ -80,16 +86,40 @@ public class Client extends Canvas implements Runnable {
 				s.render(g);
 			}
 		}
+
+		render(g);
 	}
 
 	@Override
 	public void run() {
-		BufferStrategy bs = getBufferStrategy();
-		while (GAME_RUNNING) {
-			render((Graphics2D) bs.getDrawGraphics());
-			bs.show();
+		strategy = getBufferStrategy();
+		long fpsWait = (long) (1.0 / 60 * 1000);
+		main: while (GAME_RUNNING) {
+			long renderStart = System.nanoTime();
+			updateGame();
+			// Update Graphics
+			do {
+				Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
+				if (!GAME_RUNNING) {
+					break main;
+				}
+				renderSystem(g);
+			} while (!updateScreen());
 
+			// Better do some FPS limiting here
+			long renderTime = (System.nanoTime() - renderStart) / 1000000;
+			try {
+				Thread.sleep(Math.max(0, fpsWait - renderTime));
+			} catch (InterruptedException e) {
+				Thread.interrupted();
+				break;
+			}
+			renderTime = (System.nanoTime() - renderStart) / 1000000;
+			window.setTitle(GAME_NAME + " - FPS: " + (1000 / renderTime));
 		}
+
+		window.dispose();
+		System.exit(-1);
 	}
 
 	private void startGame() {
@@ -98,9 +128,72 @@ public class Client extends Canvas implements Runnable {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(this);
 		window.setLocationRelativeTo(null);
+		window.addWindowListener(new WindowListener() {
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				try {
+					Log.dumpToFile(new File("Log.log"));
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 		window.setVisible(true);
 		createBufferStrategy(2);
 		// Start Logic & Graphic Loop
 		new Thread(this).start();
+	}
+
+	public abstract void updateGame();
+
+	private boolean updateScreen() {
+		try {
+			strategy.show();
+			Toolkit.getDefaultToolkit().sync();
+			return (!strategy.contentsLost());
+
+		} catch (NullPointerException e) {
+			return true;
+
+		} catch (IllegalStateException e) {
+			return true;
+		}
 	}
 }
